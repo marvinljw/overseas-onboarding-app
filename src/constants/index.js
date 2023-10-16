0e92fed396f4671e37c3e31e68724d9e614ae3b1
marvinljw <61826972+marvinljw@users.noreply.github.com>
2023-10-16 22:01:45 +0800

    Added some colour constants

---
+++ src/constants/index.js
@@ -0,0 +1,50 @@
+const COLORS = {
+    red: "#E73929",
+    green: "#008700",
+    blue: "#2563CC",
+  
+    lightBlue: "#E1F0FF",
+    
+    white: "#FFFFFF",
+  };
+  
+//    NOT CONFIRMS BLEOW
+  const FONT = {
+    regular: "DMRegular",
+    medium: "DMMedium",
+    bold: "DMBold",
+  };
+  
+  const SIZES = {
+    xSmall: 10,
+    small: 12,
+    medium: 16,
+    large: 20,
+    xLarge: 24,
+    xxLarge: 32,
+  };
+  
+  const SHADOWS = {
+    small: {
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      elevation: 2,
+    },
+    medium: {
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 5.84,
+      elevation: 5,
+    },
+  };
+  
+  export { COLORS, FONT, SIZES, SHADOWS };
